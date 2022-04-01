3173577100559af5329ad98986326c04a8d48470
ResinDh <ferizi10rasim@gmail.com>
2022-04-01 22:59:39 +0200

    Renis 1.0
    Deine mom

---
+++ Assets/Scipts/KingMarkSprite.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingMarkSprite : MonoBehaviour
+{
+    public GameObject player;
+
+    private SpriteRenderer _renderer;
+
+    public Rigidbody rb;
+
+    void Start()
+    {
+        InvokeRepeating("Flipping", 0.2f, 0.2f);  //0.01s delay, repeat every 0.01s
+        _renderer = GetComponent<SpriteRenderer>();
+        if (_renderer == null)
+        {
+            Debug.LogError("Player Sprite is missing a renderer");
+        }
+    }
+
+    void Flipping()
+    {
+        if(rb.velocity.magnitude >= 5)
+        {
+            if(_renderer.flipX == false)
+            {
+                _renderer.flipX = true;
+            }
+            else
+            {
+                _renderer.flipX = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        transform.LookAt(player.transform);
+    }
+}
